fee7702fe9eb487c62bd926e964e364771ad8b92
Matthias Rampke <mr@soundcloud.com>
2016-06-26 11:51:06 +0000

    remove postfix
    let people set up their own receivers.

--- prepare.sh
+++ prepare.sh
@@ -30,7 +30,7 @@ EOF
 fi
 echo '---------------------------------------'
 
-for image in golang:1.6-onbuild prom/prometheus:0.20.0 prom/alertmanager:0.2.1 google/cadvisor:v0.23.2 grafana/grafana:3.0.4 catatnight/postfix:latest
+for image in golang:1.6-onbuild prom/prometheus:0.20.0 prom/alertmanager:0.2.1 google/cadvisor:v0.23.2 grafana/grafana:3.0.4
 do
   docker pull "${image}"
 done
